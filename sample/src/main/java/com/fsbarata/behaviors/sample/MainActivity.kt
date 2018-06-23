3e9e908e4a5e51ba4ecf2190512a71d9beb809d9
FSBarata <fixedlight@gmail.com>
2018-06-23 10:32:50 +0100

    created main activity example in sample project

--- sample/src/main/java/com/fsbarata/behaviors/sample/MainActivity.kt
+++ sample/src/main/java/com/fsbarata/behaviors/sample/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.fsbarata.behaviors.sample
 
 import android.os.Bundle
-import android.app.Activity
+import com.fsbarata.behaviors.ActionDrawerActivityBehavior
 import com.fsbarata.behaviors.framework.BehaviorActivity
 
-import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : BehaviorActivity() {
+	init {
+		addBehavior(ActionDrawerActivityBehavior(
+				this,
+				R.id.drawer_layout, R.id.toolbar, R.id.nav_view,
+				R.string.navigation_drawer_open, R.string.navigation_drawer_close,
+				navigationItemSelectedListener = {
+					when (it.itemId) {
+						R.id.nav_main -> noop()
+						R.id.nav_news -> noop()
+						R.id.nav_gallery -> noop()
+					}
+					true
+				}))
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 	}
 
+	private fun noop() {}
 }
