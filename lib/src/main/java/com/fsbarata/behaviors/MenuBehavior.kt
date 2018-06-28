817278ba448512ed120a166a000c203fc0a659dd
FSBarata <fixedlight@gmail.com>
2018-06-28 18:58:25 +0100

    changed MenuBehavior arguments to map of ids to click listeners

--- lib/src/main/java/com/fsbarata/behaviors/MenuBehavior.kt
+++ lib/src/main/java/com/fsbarata/behaviors/MenuBehavior.kt
@@ -1,7 +1,6 @@
 package com.fsbarata.behaviors
 
 import android.app.Activity
-import android.support.annotation.IdRes
 import android.support.annotation.MenuRes
 import android.view.Menu
 import android.view.MenuInflater
@@ -11,8 +10,9 @@ import com.fsbarata.behaviors.framework.AbstractLifecycleBehavior
 open class MenuBehavior(
 		private val activity: Activity?,
 		@param:MenuRes private val menuRes: Int,
-		vararg private val itemsDetails: MenuItemDetails
+		vararg idClickListeners: Pair<Int, (MenuItem) -> Unit>
 ) : AbstractLifecycleBehavior() {
+	private val itemClickListenerMap = idClickListeners.toMap()
 
 	override fun onBehaviorAttached() {
 		if (activity?.window != null) {
@@ -28,20 +28,10 @@ open class MenuBehavior(
 
 	override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater): Boolean {
 		menuInflater.inflate(menuRes, menu)
-		itemsDetails.forEach {
-			menu.findItem(it.id)?.apply { it.title?.let { title = it } }
-		}
 		return true
 	}
 
 	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		return itemsDetails.find { it.id == item.itemId }
-				?.run { onClickListener(item) } != null
+		return itemClickListenerMap[item.itemId]?.also { it(item) } != null
 	}
-
-	data class MenuItemDetails(
-			@IdRes val id: Int,
-			val title: String? = null,
-			val onClickListener: (MenuItem) -> Unit
-	)
 }
