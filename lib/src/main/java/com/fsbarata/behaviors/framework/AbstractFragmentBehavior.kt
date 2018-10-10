6b45ff6ed5be0f0ec1427e97b2f9c90d7b955d6e
FSBarata <fixedlight@gmail.com>
2018-10-10 09:15:09 +0100

    fixed AbstractFragmentBehavior to include new onActivityCreated callback

--- lib/src/main/java/com/fsbarata/behaviors/framework/AbstractFragmentBehavior.kt
+++ lib/src/main/java/com/fsbarata/behaviors/framework/AbstractFragmentBehavior.kt
@@ -1,9 +1,12 @@
 package com.fsbarata.behaviors.framework
 
 import android.content.Context
+import android.os.Bundle
 import android.view.View
 
 abstract class AbstractFragmentBehavior : AbstractLifecycleBehavior(), IFragmentBehavior {
+	override fun onActivityCreated(savedInstanceState: Bundle?) {}
+
 	override fun onViewCreated(view: View) {}
 
 	override fun onDestroyView() {}
