683ebdb6d8a22253bb9d4c42890135da516f9214
fixedlight <fixedlight@gmail.com>
2019-03-12 07:22:27 +0000

    added fragment and activity flavors to MenuBehavior

--- lib/src/main/java/com/fsbarata/behaviors/MenuBehavior.kt
+++ lib/src/main/java/com/fsbarata/behaviors/MenuBehavior.kt
@@ -2,27 +2,41 @@ package com.fsbarata.behaviors
 
 import android.app.Activity
 import android.support.annotation.MenuRes
+import android.support.v4.app.Fragment
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import com.fsbarata.behaviors.framework.AbstractLifecycleBehavior
 
-open class MenuBehavior(
-		private val activity: () -> Activity?,
+class MenuBehavior private constructor(
+		private val activity: Activity?,
+		private val fragment: Fragment?,
 		@param:MenuRes private val menuRes: Int,
 		vararg idClickListeners: Pair<Int, (MenuItem) -> Unit>
 ) : AbstractLifecycleBehavior() {
 	private val itemClickListenerMap = idClickListeners.toMap()
 
+	constructor(
+			activity: Activity,
+			@MenuRes menuRes: Int,
+			vararg idClickListeners: Pair<Int, (MenuItem) -> Unit>
+	): this(activity, null, menuRes, *idClickListeners)
+
+	constructor(
+			fragment: Fragment,
+			@MenuRes menuRes: Int,
+			vararg idClickListeners: Pair<Int, (MenuItem) -> Unit>
+	): this(null, fragment, menuRes, *idClickListeners)
+
 	override fun onBehaviorAttached() {
-		val activity = activity() ?: return
+		val activity = activity ?: fragment?.activity ?: return
 		if (activity.window != null) {
 			activity.invalidateOptionsMenu()
 		}
 	}
 
 	override fun onBehaviorDetached() {
-		val activity = activity() ?: return
+		val activity = activity ?: fragment?.activity ?: return
 		if (activity.window != null) {
 			activity.invalidateOptionsMenu()
 		}
